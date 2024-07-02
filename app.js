c8d826a10a538635d6f5d098817357cb7fe62435
Wil Bowes <wilb@checkpoint.com>
2024-07-02 17:54:47 +1000

    changes as far as the eye can see!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Wooohooo! It works!'));
+app.get('/', (req, res) => res.send('Yet another change...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
