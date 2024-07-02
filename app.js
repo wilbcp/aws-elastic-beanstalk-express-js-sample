5d3632f4c16cd20b1dfdff9848ac33fb4f111489
Wil Bowes <wilb@checkpoint.com>
2024-07-02 17:25:55 +1000

    change message2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - Here I am!'));
+app.get('/', (req, res) => res.send('Wooohooo! It works!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
